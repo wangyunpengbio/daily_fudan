40abf6b4151ec52658e22b2156661fc93b44779f
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2022-03-31 21:15:50 +0800

    Update dailyFudan.py

--- dailyFudan.py
+++ dailyFudan.py
@@ -282,9 +282,11 @@ if __name__ == '__main__':
 
     # 再检查一遍
     if daily_fudan.check():
-        iy_info("平安复旦：今日已提交", gl_info)
+        logging.info("OK")
+#         iy_info("平安复旦：今日已提交", gl_info)
     else:
-        iy_info("平安复旦：本次提交失败", gl_info)
+        iy_info("请打卡", gl_info)
+#         iy_info("平安复旦：本次提交失败", gl_info)
 
     daily_fudan.close()
     sys_exit()
