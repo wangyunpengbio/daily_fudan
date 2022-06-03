0f9e8669a0bc4c927e2403d9398819877f07fa8c
wangyunpeng_bio <wangyunpeng_bio@qq.com>
2022-06-03 09:53:15 +0800

    Update dailyFudan.py

--- dailyFudan.py
+++ dailyFudan.py
@@ -273,7 +273,7 @@ if __name__ == '__main__':
 
     if daily_fudan.check():
         logging.info("OK")
-#         iy_info("平安复旦：今日已提交", gl_info)
+        iy_info("平安复旦：今日已提交", gl_info)
         sys_exit()
 
     def captcha_info(message):
@@ -284,10 +284,10 @@ if __name__ == '__main__':
     # 再检查一遍
     if daily_fudan.check():
         logging.info("OK")
-#         iy_info("平安复旦：今日已提交", gl_info)
+        iy_info("平安复旦：今日已提交", gl_info)
     else:
         iy_info("请打卡", gl_info)
-#         iy_info("平安复旦：本次提交失败", gl_info)
+        iy_info("平安复旦：本次提交失败", gl_info)
 
     daily_fudan.close()
     sys_exit()
